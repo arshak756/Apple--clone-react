2fb786366575f126ee6280772b559371e2e89e72
arshak756 <mohammedarshak793@gmail.com>
2025-05-11 23:18:48 +0500

    updated changes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import Article from './components/Article'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Table from './components/Table'
+
+function App() {
+  
+
+  return (
+    <>
+     <Navbar/>
+     <Header/>
+     <Hero/>
+     <Article/>
+     <Table/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
